1fefaaa6ee8b185ed2fa32af85e321a9914a17d8
ducheanidi-designs <ducheanidi@gmail.com>
2026-03-21 06:13:12 +0100

    practice for assign.1

--- Assets/PlayerVariables.cs
+++ Assets/PlayerVariables.cs
@@ -10,6 +10,9 @@ public class PlayerVariables : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      
+        Debug.Log(playerName + " - Player Name");
+        Debug.Log(playerAge + " - Player Age");
+        Debug.Log(playerSpeed + " - Game Speed");
+        Debug.Log(isAlive + " - Game Health");
     }
 }
