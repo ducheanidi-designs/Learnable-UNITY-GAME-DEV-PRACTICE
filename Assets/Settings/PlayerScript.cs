45c08697dfe3b391cea3e96be882809bbd93321f
ducheanidi-designs <ducheanidi@gmail.com>
2026-03-22 16:36:01 +0100

    Modification of variables
    PlayerName: D.Ichakaike
    PlayerAge: 1000

--- Assets/Settings/PlayerScript.cs
+++ Assets/Settings/PlayerScript.cs
@@ -5,14 +5,13 @@ public class PlayerScript : MonoBehaviour
     public string playerName = "Ndu Champ"; // This box (variable) called playerName holds the text "Ndu Champ".
     public int playerAge = 70; // This box (variable) called playerAge holds the number "70".
     public float playerHealth = 100.5f; // This box (variable) called playerHealth holds the text "100.5f".
-    private int playerDamage = 20; // This box (variable) called playerDamage holds the number "20".
+    public int playerDamage = 20; // This box (variable) called playerDamage holds the number "20".
     private bool isAlive = true; // This box (variable) called isAlive hold the true/false vaue.
 
     void Awake()
     {
-        Debug.Log("player name is -" + playerName);
-        Debug.Log("player age is -" + playerAge);
-        Debug.Log("player health is -" + playerHealth);
+        Debug.Log("Player name is -" + playerName);
+        Debug.Log("Player health is -" + playerHealth);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
