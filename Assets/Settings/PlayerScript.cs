d9984277d06feec91a0bc86d5aad2c613eff7886
ducheanidi-designs <ducheanidi@gmail.com>
2026-03-22 06:33:46 +0100

    Variables for Player

---
+++ Assets/Settings/PlayerScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public string playerName = "Ndu Champ"; // This box (variable) called playerName holds the text "Ndu Champ".
+    public int playerAge = 70; // This box (variable) called playerAge holds the number "70".
+    public float playerHealth = 100.5f; // This box (variable) called playerHealth holds the text "100.5f".
+    private int playerDamage = 20; // This box (variable) called playerDamage holds the number "20".
+    private bool isAlive = true; // This box (variable) called isAlive hold the true/false vaue.
+
+    void Awake()
+    {
+        Debug.Log("player name is -" + playerName);
+        Debug.Log("player age is -" + playerAge);
+        Debug.Log("player health is -" + playerHealth);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
