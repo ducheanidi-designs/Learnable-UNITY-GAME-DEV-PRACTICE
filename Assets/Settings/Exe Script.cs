0ff7490451ffd56a6c0816b61509901328154af5
ducheanidi-designs <ducheanidi@gmail.com>
2026-03-22 05:54:30 +0100

    Excution order Prac.

---
+++ Assets/Settings/Exe Script.cs
@@ -0,0 +1,15 @@
+ using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake was called");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Start was called");
+    }
+}
