fec005e77f34121e5cad3366b2d12fcd2ee11b90
ducheanidi-designs <ducheanidi@gmail.com>
2026-03-23 02:13:53 +0100

    WeaponScript

---
+++ Assets/Settings/WeaponScript.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    string[] weapons = { "oria", "egbe", "Ogbun'igwe" };
+
+    int[] weapons2 = {1,2,3,3,8};
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("First Weapon: " + weapons[0]);
+
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            Debug.Log("weapon: " + i + ": " + weapons[i]);
+        }
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
