93721450f9b44150f7954f5b878f3e2fcb1c67f9
ducheanidi-designs <ducheanidi@gmail.com>
2026-03-24 05:49:16 +0100

    Script updates

--- Assets/Settings/EnemyScript.cs
+++ Assets/Settings/EnemyScript.cs
@@ -2,19 +2,63 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    string[] enemy = { "Oria", "Posion", "Ojuju", "med" };
+    string[] enemies = { "Zombie", "Alien", "Robot", "Boss", "Med" };
 
     int[] enemyNos = {1,3,5,6,8};
+
+    int playerHealth = 100;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("First Enemy: " + enemy[0]);
+        Debug.Log("First Enemy: " + enemies[0]);
 
-        for (int i = 0; i < enemy.Length; i++)
+        for (int i = 0; i < enemies.Length; i++)
         {
-            Debug.Log("Enemy: " + i + ": " + enemy[i]);
+            Debug.Log("Enemy: " + i + ": " + enemies[i]);
+        }
+
+        foreach (string enemy in enemies)
+        {
+            AttackEnemy(enemy);
         }
     }
+
+    void AttackEnemy(string enemyName)
+    {
+        if (enemyName == "Med")
+        {
+            Debug.Log("Found a Med! Healing...");
+
+            playerHealth += 20;
+
+            Debug.Log("Player health is now " + playerHealth);
+        }
+        else
+        {
+            Debug.Log("Fighting" + enemyName + "...");
+
+            playerHealth -= 20;
+
+            Debug.Log("Player health is now " + playerHealth);
+        }
+    
+        void CheckPlayerHealth()
+        {
+            if (playerHealth > 50)
+            {
+                Debug.Log("Player is strong");
+            }
+            else if (playerHealth <= 50)
+            {
+                Debug.Log("Player is weak");
+            }
+            else if (playerHealth <= 0)
+            {
+                Debug.Log("Player is dead and Stop the game");
+            }
+        }
+
+    }    
 
 }
--- Assets/Settings/PlayerScript.cs
+++ Assets/Settings/PlayerScript.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public string playerName = "Ndu Champ"; // This box (variable) called playerName holds the text "Ndu Champ".
+    public string playerName = "Ndu D.Champ"; // This box (variable) called playerName holds the text "Ndu D.Champ".
     public int playerAge = 70; // This box (variable) called playerAge holds the number "70".
-    public float playerHealth = 100.5f; // This box (variable) called playerHealth holds the text "100.5f".
-    public int playerDamage = 20; // This box (variable) called playerDamage holds the number "20".
+    private int playerHealth = 100; // This box (variable) called playerHealth holds the text "100".
+    private int playerDamage = 20; // This box (variable) called playerDamage holds the number "20".
     private bool isAlive = true; // This box (variable) called isAlive hold the true/false vaue.
 
     void Awake()
     {
         Debug.Log("Player name is -" + playerName);
-        Debug.Log("Player health is -" + playerHealth);
+        Debug.Log("Player age is -" + playerAge);
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
